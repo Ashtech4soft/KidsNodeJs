8004dbcb774856cd3bf6a2570cea73a9ac2c8eef
ashtech4soft <ash.tech4soft@gmail.com>
2025-03-15 00:30:43 +0530

    port change commit

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(cors({
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Setup for the server port number
-const port = 4000;
+const PORT = process.env.PORT || 5000;
 
 // Routes Configuration
 app.use('/users', userRoutes);
