185f29acbe5827a28a4df332dc7bb77e38be2779
ashtech4soft <ash.tech4soft@gmail.com>
2025-03-15 00:28:30 +0530

    second commit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cookieParser())
 
 // Enable CORS
 app.use(cors({
-    origin: ["http://localhost:5173", "https://kidsreactjs.onrender.com"],
+    origin: ["https://kidsreactjs.onrender.com"],
     credentials: true,
 }));
 
