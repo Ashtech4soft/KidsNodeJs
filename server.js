ee273992f1ae864f9e944ae32e745ba2dc6eca4b
ashtech4soft <ash.tech4soft@gmail.com>
2025-03-15 19:23:43 +0530

    Updated code with fixes and improvements

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(cors({
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Setup for the server port number
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // Routes Configuration
 app.use('/users', userRoutes);
